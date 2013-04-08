f3d764aadf63dabcacbaccd9fbab761a6a387a2b
ralzakark <alessandro.caputo@gmail.com>
2013-04-08 18:47:34 +0200

    modified:   app/main/index/homepage.lua modified:   app/main/index/welcome.lua modified:   static/js/jquery-1.9.1.min.js modified:   static/js/position.js

--- app/main/index/welcome.lua
+++ app/main/index/welcome.lua
@@ -14,21 +14,30 @@ ui.container{ attr = { id  = "welcome_middle_box" }, content = function()
       ui.tag{ 
         tag="p", 
         attr = {class = "welcome_text_l"}, 
-        content= _("#{realname}, your last login was on #{last_login_date} at #{last_login_time} from #{city}, #{region}.", {
+        content= _("#{realname}, your last login was on #{last_login_date} at #{last_login_time}", {
           realname = member.realname,
           last_login_date =  format.date(member.last_login),
           last_login_time = format.time(member.last_login)
         }) 
       }
+      local lastLogin = member:get_last_login_data()
+      if lastLogin and lastLogin.geolat and lastLogin.geolng and lastLogin.login_time then
+        if lastLogin.geolat and lastLogin.geolng then
+          slot.put('<span id="location" />')
+          ui.script{ script = 'codeLatLng('..lastLogin.geolat..','..lastLogin.geolng..',"location",_"from  ");'}
+        end
+      else
+        trace.debug('Cannot retrieve coordinates')
+      end
       slot.put("<br />")
       ui.tag{ tag="p", attr = { class = "welcome_text_xl"}, content= _"CHOOSE THE ASSEMBLY YOU WANT TO PARTECIPATE TO:"}
 
       ui.link{ attr = {class = "no_deco"}, module="index", view="index", content=function()
-        ui.container{ attr = {id = "welcome_middle_box_left", class="button" }, content=function()
+        ui.container{ attr = {id = "welcome_middle_box_left", class="button orange menuButton" }, content=function()
           ui.tag{ tag="p", attr = {class = "button_text" }, content= _"REGIONE LAZIO ASSEMBLY"}
         end}
       end}
-      ui.container{ attr = {id = "welcome_middle_box_right", class="button" }, content=function()
+      ui.container{ attr = {id = "welcome_middle_box_right", class="button orange menuButton" }, content=function()
         ui.tag{ tag="p", attr = {class = "button_text" }, content= _"5 STARS MOVEMENT LAZIO INTERNAL ASSEMBLY"}
       end}
       ui.tag{  tag="img", attr = { id = "welcome_parlamento_left", src="../static/parlamento_icon_small.png", alt="Icona Parlamento"} }
@@ -40,7 +49,7 @@ ui.container{ attr = {id = "welcome_footer_container" }, content=function()
   ui.tag{  tag="img", attr = { id = "welcome_footer_box_left" , src="../static/simbolo_movimento.png", alt="Movimento 5 Stelle"} }
   ui.tag{ tag="p", attr = { id = "welcome_footer_box_middle" , class = "welcome_text_xl" }, content= _"ARE YOU A LAZIO CITIZEN AND YOU WANT TO REGISTER? HERE'S HOW TO DO IT:"}
   ui.tag{  tag="img", attr = { id = "arrow_right" , src="../static/arrow_right.png", alt="Freccia destra"} }
-  ui.container{ attr = {id = "welcome_footer_box_right", class="button orange brackets" }, content=function()
+  ui.container{ attr = {id = "welcome_footer_box_right", class="button orange menuButton" }, content=function()
     ui.tag{ tag="p", attr = {class = "button_text" }, content= _"GUIDA ALLA ISCRIZIONE"}
   end}
 end}
