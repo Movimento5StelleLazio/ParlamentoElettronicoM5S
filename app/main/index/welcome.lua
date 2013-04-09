02de2232a1d47d0699f2dc774cbca0c6cc59e32e
ralzakark <alessandro.caputo@gmail.com>
2013-04-09 10:42:45 +0200

    Modifiche varie: aggiunti trace, onload x JS, stili modified:   app/main/index/_action/login.lua modified:   app/main/index/welcome.lua modified:   static/js/position.js modified:   static/m5s.css

--- app/main/index/welcome.lua
+++ app/main/index/welcome.lua
@@ -7,39 +7,42 @@ ui.container{
   end
 }
 
-locale.set{date_format="DD/MM/YYYY"}
 
 ui.container{ attr = { id  = "welcome_middle_box" }, content = function()
-      ui.tag{ tag="p", attr = { class = "welcome_text_l"}, content= _("WELCOME #{realname}.", {realname = member.realname}) }
-      ui.container{ attr = { class = "welcome_text_l" }, content = function ()
-        ui.tag{
-          tag="span",
-          attr = {class = "welcome_text_l"},
-          content= _("#{realname}, your last login was on #{last_login_date} at #{last_login_time}", {
-            realname = member.realname,
-            last_login_date = format.date(member.last_login),
-            last_login_time = format.time(member.last_login)
-          })
-        }
-        ui.tag{tag="span",attr = { id = "location", class = "welcome_text_l"}}
-      end}
-
-      ui.script{ static = "js/position.js" }
-      local lastLogin = member:get_last_login_data()
+  if app.session.member_id then     
+    locale.set{date_format="DD/MM/YYYY"}
+ 
+    -- Codifica coordinate memorizzate in member_login
+    local lastLogin = member:get_last_login_data()
       if lastLogin and lastLogin.geolat and lastLogin.geolng and lastLogin.login_time then
-        ui.script{ script = 'codeLatLng('..lastLogin.geolat..','..lastLogin.geolng..',"location",_"from ");'}
+        trace.debug('Calling codelatlng('..lastLogin.geolat..','..lastLogin.geolng..',"location","'.._"from "..'");')
+        ui.script{ static = "js/position.js" }
+        ui.script{ script = 'codelatlng('..lastLogin.geolat..','..lastLogin.geolng..',"location","'.._"from "..'");'}
       else
-        trace.debug('Cannot retrieve coordinates')
+        trace.debug('Cannot retrieve coordinates from database')
       end
+    ui.tag{ tag="p", attr = { class = "welcome_text_l"}, content= _("WELCOME #{realname}.", {realname = member.realname}) }
+    ui.container{ attr = { class = "welcome_text_l" }, content = function ()
+      ui.tag{
+        tag="span",
+        attr = {class = "welcome_text_l"},
+        content= _("#{realname}, your last login was on #{last_login_date} at #{last_login_time}", {
+          realname = member.realname,
+          last_login_date = format.date(member.last_login),
+          last_login_time = format.time(member.last_login)
+        })
+      }
+      ui.tag{tag="span",attr = { id = "location", class = "welcome_text_l"}}
+    end}
+  end
+
       slot.put("<br />")
       ui.tag{ tag="p", attr = { class = "welcome_text_xl"}, content= _"CHOOSE THE ASSEMBLY YOU WANT TO PARTECIPATE TO:"}
 
-      ui.link{ attr = {class = "no_deco"}, module="index", view="homepage", content=function()
-        ui.container{ attr = {id = "welcome_middle_box_left", class="button orange menuButton" }, content=function()
-          ui.tag{ tag="p", attr = {class = "button_text" }, content= _"REGIONE LAZIO ASSEMBLY"}
-        end}
+      ui.link{ attr = {id = "welcome_middle_box_left", class="button orange menuButton"}, module="index", view="homepage", content=function()
+        ui.tag{ tag="p", attr = {class = "button_text" }, content= _"REGIONE LAZIO ASSEMBLY"}
       end}
-      ui.container{ attr = {id = "welcome_middle_box_right", class="button orange menuButton" }, content=function()
+      ui.link{ attr = {id = "welcome_middle_box_right", class="button orange menuButton" }, content=function()
         ui.tag{ tag="p", attr = {class = "button_text" }, content= _"5 STARS MOVEMENT LAZIO INTERNAL ASSEMBLY"}
       end}
       ui.tag{  tag="img", attr = { id = "welcome_parlamento_left", src="../static/parlamento_icon_small.png", alt="Icona Parlamento"} }
