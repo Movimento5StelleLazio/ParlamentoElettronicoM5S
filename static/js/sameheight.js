bfcf73004b4824bfde698110005bd3b0e6195219
ralzakark <alessandro.caputo@gmail.com>
2013-05-21 15:24:29 +0200

    deleted:    static/js/sameheight.js

--- static/js/sameheight.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var highestBox = 0;
-  $('.sameheight', this).each(function(){
-    if($(this).height() > highestBox) 
-      highestBox = $(this).height(); 
-    });  
-  $('.sameheight',this).height(highestBox);
-});
