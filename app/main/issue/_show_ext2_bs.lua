e07ad083d66773f1bed158e95588a62fbd3b9a46
ralzakark <alessandro.caputo@gmail.com>
2013-06-22 19:24:42 +0200

    Nuova phases bar 	modified:   app/main/issue/_show_ext2_bs.lua 	new file:   app/main/issue/phasesbar.lua 	modified:   static/m5s_bs.css 	new file:   static/svg/phases_bar_it.svg 	new file:   static/svg/phases_bar_notxt.svg

--- app/main/issue/_show_ext2_bs.lua
+++ app/main/issue/_show_ext2_bs.lua
@@ -23,48 +23,27 @@ elseif vote_comment_able then
 end  
 
 
---local class = "issue_ext2"
---if issue.is_interested then
---  class = class .. " interested"
---elseif issue.is_interested_by_delegation_to_member_id then
---  class = class .. " interested_by_delegation"
---end
-
-local arrow_offset = 31
-local admission_offset, discussion_offset, verification_offset, voting_offset, committee_offset, committee_voting_offset, finished_offset = 55,58,72,64,58,64,66 
-
 -- Uncomment the following to use svgz instead of svg
 local svgz = ""
 --local svgz = "z"
 
 if issue.state == "admission" then
   event_name = _"New issue"
-  admission_offset = 37
 elseif issue.state == "discussion" then
   event_name = _"Discussion started"
   event_image = "comments.png"
-  discussion_offset = 37
-  arrow_offset = 98
 elseif issue.state == "verification" then
   event_name = _"Verification started"
   event_image = "lock.png"
-  verification_offset = 53
-  arrow_offset = 161
 elseif issue.state == "voting" then
   event_name = _"Voting started"
   event_image = "email_open.png"
-  voting_offset = 46
-  arrow_offset = 224
 elseif issue.state == "committee" then
   event_name = _"Committee started"
   event_image = "lock.png"
-  committee_offset = 49
-  arrow_offset = 298
 elseif issue.state == "committee_voting" then
   event_name = _"Committee voting started"
   event_image = "email_open.png"
-  committee_voting_offset = 49
-  arrow_offset = 361
 elseif issue.closed  then
   event_image = "cross.png"
   if issue.state == "finished_with_winner" then 
@@ -84,8 +63,6 @@ elseif issue.closed  then
   elseif issue.state == 'canceled_no_initiative_admitted' then
     event_name = _"Canceled (no initiative admitted)"
   end
-  finished_offset = 49
-  arrow_offset = 432
 end
 
 ui.container{ attr = { class = "row-fluid"}, content = function()
@@ -127,17 +104,9 @@ ui.container{ attr = { class = "row-fluid"}, content = function()
         end }
       end }
       ui.container{ attr = { class = "span9"}, content = function()
-        ui.container{ attr = { id = "phases_box_"..issue.id, class = "phases_box"}, content = function()
-          ui.image{  attr = { id = "phase_arrow_"..issue.id, class = "phase_arrow", style = "margin-left: "..arrow_offset.."px;" }, static="svg/phase_arrow.svg"..svgz }
-          ui.image{  attr = { id = "phases_bar_"..issue.id, class = "phases_bar" }, static = "svg/phases_bar.svg"..svgz }
-          ui.image{  attr = { id = "admission_"..issue.id, class = "admission", style = "margin-top: "..admission_offset.."px;" }, static="svg/admission.svg"..svgz }
-          ui.image{  attr = { id = "discussion_"..issue.id, class = "discussion", style = "margin-top: "..discussion_offset.."px;" }, static="svg/discussion.svg"..svgz }
-          ui.image{  attr = { id = "verification_"..issue.id, class = "verification", style = "margin-top: "..verification_offset.."px;" }, static="svg/verification.svg"..svgz }
-          ui.image{  attr = { id = "voting_"..issue.id, class = "voting", style = "margin-top: "..voting_offset.."px;" }, static="svg/voting.svg"..svgz }
-          ui.image{  attr = { id = "committee_"..issue.id, class = "committee", style = "margin-top: "..committee_offset.."px;" }, static="svg/committee.svg"..svgz }
-          ui.image{  attr = { id = "committee_voting_"..issue.id, class = "committee_voting", style = "margin-top: "..committee_voting_offset.."px;" }, static="svg/voting.svg"..svgz }
-          ui.image{  attr = { id = "finished_"..issue.id, class = "finished", style = "margin-top: "..finished_offset.."px;" }, static="svg/finished.svg"..svgz }
-        end}
+        ui.container{ attr = { class = "pull-right"}, content = function()
+          execute.view{ module = "issue", view = "phasesbar", params = { sate=issue.state } }       
+        end }
       end }
     end }
 
---
+++ app/main/issue/phasesbar.lua
@@ -0,0 +1,48 @@
+slot.set_layout("m5s_bs")
+local state=param.get("state") or "closed"
+
+local arrow_offset = 16
+local admission_offset, discussion_offset, verification_offset, voting_offset, committee_offset, committee_voting_offset, finished_offset 
+
+-- Uncomment the following to use svgz instead of svg
+local svgz = ""
+--local svgz = "z"
+
+if state == "admission" then
+  admission_offset = "margin-top: 29px;"
+elseif state == "discussion" then
+  discussion_offset = "margin-top: 25px;"
+  arrow_offset = 83
+elseif state == "verification" then
+  verification_offset = "margin-top: 42px;height: 27px;margin-left: 168px;"
+  arrow_offset = 146
+elseif state == "voting" then
+  voting_offset = "margin-top: 35px;"
+  arrow_offset = 209
+elseif state == "committee" then
+  committee_offset = "margin-top: 36px;"
+  arrow_offset = 281
+elseif state == "committee_voting" then
+  committee_voting_offset = "margin-top: 38px;"
+  arrow_offset = 344
+elseif state == "closed"  then
+  finished_offset = "margin-top: 38px;margin-left: 432px;height: 41px;"
+  arrow_offset = 416
+end
+
+ui.container{ attr = { class = "row-fluid"}, content = function()
+  ui.container{ attr = { class = "span12"}, content = function()
+    ui.container{ attr = { class = "phases_box"}, content = function()
+      ui.image{  attr = { class = "phase_arrow", style = "margin-left: "..arrow_offset.."px;" }, static="svg/phase_arrow.svg"..svgz }
+      ui.image{  attr = { class = "phases_bar" }, static = "svg/phases_bar_it.svg"..svgz }
+
+      ui.image{  attr = { class = "admission", style = admission_offset }, static="svg/admission.svg"..svgz }
+      ui.image{  attr = { class = "discussion", style = discussion_offset }, static="svg/discussion.svg"..svgz }
+      ui.image{  attr = { class = "verification", style = verification_offset }, static="svg/verification.svg"..svgz }
+      ui.image{  attr = { class = "voting", style = voting_offset }, static="svg/voting.svg"..svgz }
+      ui.image{  attr = { class = "committee", style = committee_offset }, static="svg/committee.svg"..svgz }
+      ui.image{  attr = { class = "committee_voting", style = committee_voting_offset }, static="svg/voting.svg"..svgz }
+      ui.image{  attr = { class = "finished", style = finished_offset }, static="svg/finished.svg"..svgz }
+    end}
+  end }
+end }
