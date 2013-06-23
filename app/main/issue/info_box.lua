77f1ddc1547c4a3cca2f9a6d133d1073c7bccb06
ralzakark <alessandro.caputo@gmail.com>
2013-06-23 23:06:00 +0200

    Fittext per la info_box

--- app/main/issue/info_box.lua
+++ app/main/issue/info_box.lua
@@ -41,42 +41,46 @@ end
 
 
 
-      ui.container{ attr = { class = "issue_state_info_box2"}, content = function()
-        ui.container{ attr = { class = "row-fluid"}, content = function()
-          ui.container{ attr = { class = "span12 text-center"}, content = function()
-            ui.heading{ level=6, content = function()
-              if event_image then
-                ui.image{ attr = { class = ""}, static = "icons/16/" .. event_image }
-              end
-              slot.put(event_name or "")
-            end }
-
-          end }
-        end }
-        ui.container{ attr = { class = "row-fluid"}, content = function()
-          ui.container{ attr = { class = "span12 text-center"}, content = function()
-            ui.tag{ tag = "p", content = function()
-              if issue.closed then
-                slot.put(" &middot; ")
-                ui.tag{ content = format.interval_text(issue.closed_ago, { mode = "ago" }) }
-              elseif issue.state_time_left then
-                slot.put(" &middot; ")
-                if issue.state_time_left:sub(1,1) == "-" then
-                  if issue.state == "admission" then
-                    ui.tag{ content = _("Discussion starts soon") }
-                  elseif issue.state == "discussion" then
-                    ui.tag{ content = _("Verification starts soon") }
-                  elseif issue.state == "verification" then
-                    ui.tag{ content = _("Voting starts soon") }
-                  elseif issue.state == "voting" then
-                    ui.tag{ content = _("Counting starts soon") }
-                  end
-                else
-                  ui.tag{ content = format.interval_text(issue.state_time_left, { mode = "time_left" }) }
-                end
-              end
-            end }
-          end }
-        end }
+ui.container{ attr = { class = "issue_state_info_box2"}, content = function()
+  ui.container{ attr = { class = "row-fluid"}, content = function()
+    ui.container{ attr = { class = "span12"}, content = function()
+      ui.heading{ level=6, attr={class="fittext_info_box1"}, content = function()
+        if event_image then
+          ui.image{ attr = { class = ""}, static = "icons/16/" .. event_image }
+        end
+        slot.put(event_name or "")
       end }
+
+    end }
+  end }
+  ui.container{ attr = { class = "row-fluid"}, content = function()
+    ui.container{ attr = { class = "span12"}, content = function()
+      ui.tag{ tag = "p", attr={class="fittext_info_box2"}, content = function()
+        if issue.closed then
+          slot.put(" &middot; ")
+          ui.tag{ content = format.interval_text(issue.closed_ago, { mode = "ago" }) }
+        elseif issue.state_time_left then
+          slot.put(" &middot; ")
+          if issue.state_time_left:sub(1,1) == "-" then
+            if issue.state == "admission" then
+              ui.tag{ content = _("Discussion starts soon") }
+            elseif issue.state == "discussion" then
+              ui.tag{ content = _("Verification starts soon") }
+            elseif issue.state == "verification" then
+              ui.tag{ content = _("Voting starts soon") }
+            elseif issue.state == "voting" then
+              ui.tag{ content = _("Counting starts soon") }
+            end
+          else
+            ui.tag{ content = format.interval_text(issue.state_time_left, { mode = "time_left" }) }
+          end
+        end
+      end }
+    end }
+  end }
+end }
+
+ui.script{static = "js/jquery.fittext.js"}
+ui.script{script = "jQuery('.fittext_info_box1').fitText(1.0, {minFontSize: '12px', maxFontSize: '22px'}); " }
+ui.script{script = "jQuery('.fittext_info_box2').fitText(1.0, {minFontSize: '10px', maxFontSize: '18px'}); " }
 
