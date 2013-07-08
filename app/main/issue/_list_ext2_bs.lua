3b5704d7deb493a16c58b8c5b4a00f5a26371256
root <root@debian>
2013-07-08 14:32:18 +0200

    Redone query per homepage

--- app/main/issue/_list_ext2_bs.lua
+++ app/main/issue/_list_ext2_bs.lua
@@ -6,9 +6,15 @@ local desc =  param.get("desc", atom.boolean)
 local interest = param.get("interest")
 local scope = param.get("scope")
 local view = param.get("view")
+local list = param.get("list")
+local for_details = param.get("for_details",atom.boolean)
 local ftl_btns = param.get("ftl_btns",atom.boolean)
 
 
+if list == "proposals" then
+  issues_selector:join("initiative", nil, "initiative.issue_id = issue.id")
+  issues_selector:join("current_draft", nil, {"current_draft.initiative_id = initiative.id AND current_draft.author_id = ?", app.session.member.id })
+end
 
 ui.paginate{
   per_page = tonumber(param.get("per_page") or 25),
@@ -20,18 +26,30 @@ ui.paginate{
     if #issues == 0 then
       ui.container{ attr = { class = "row-fluid" }, content = function()
         ui.container{ attr = { class = "span12 text-center" }, content = function()
-          ui.heading{level=4, content =_"There are no issues that match the selection criteria."}
+          if list == "voted" then
+             ui.heading{level=4, content =_"You didn't vote any issue yet."}
+          elseif list == "proposals" then
+             ui.heading{level=4, content =_"You didn't create any issue yet."}
+          else
+             ui.heading{level=4, content =_"There are no issues that match the selection criteria."}
+          end
         end }
       end }
     end
-
+    
+    local target = "_show_ext2_bs"
+    if list == "voted" or list == "proposals" then
+      target = "_show_ext_bs"
+    else
+      target = "_show_ext2_bs"
+    end
 
     ui.container{ attr = { class = "issues" }, content = function()
       for i, issue in ipairs(issues) do
-        execute.view{ module = "issue", view = "_show_ext2_bs",
+        execute.view{ module = "issue", view = target,
           params = {
             issue = issue,
-            for_listing = true,
+    --        for_listing = true,
             for_member = member,
             state = state,
             orderby = orderby,
@@ -39,6 +57,7 @@ ui.paginate{
             interest = interest,
             scope = scope,
             view = view,
+            list = list,
             ftl_btns = ftl_btns
           }
         }
--- app/main/issue/_show_ext_bs.lua
+++ app/main/issue/_show_ext_bs.lua
@@ -1,15 +1,16 @@
 local issue = param.get("issue", "table")
 local initiative_limit = param.get("initiative_limit", atom.integer)
 local for_member = param.get("for_member", "table")
-local for_listing = param.get("for_listing", atom.boolean)
-local for_initiative = param.get("for_initiative", "table")
-local for_initiative_id = for_initiative and for_initiative.id or nil
+--local for_listing = param.get("for_listing", atom.boolean)
+--local for_initiative = param.get("for_initiative", "table")
+--local for_initiative_id = for_initiative and for_initiative.id or nil
 local state = param.get("state")
 local orderby = param.get("orderby") or ""
 local desc =  param.get("desc", atom.boolean)
 local interest = param.get("interest")
 local scope = param.get("scope")
-local view = param.get("view")
+--local view = param.get("view")
+local list = param.get("list")
 local ftl_btns = param.get("ftl_btns",atom.boolean)
 
 
@@ -44,24 +45,26 @@ ui.container{ attr = { class = "row-fluid"}, content = function()
 
         ui.container{attr = {class="row-fluid"}, content =function()
           ui.container{attr = {class="span12"}, content =function()
-            local initiatives_selector = issue:get_reference_selector("initiatives")
-            local highlight_string = param.get("highlight_string")
-            if highlight_string then
-              initiatives_selector:add_field( {'"highlight"("initiative"."name", ?)', highlight_string }, "name_highlighted")
-            end
+ --           local initiatives_selector = issue:get_reference_selector("initiatives")
+  --          local highlight_string = param.get("highlight_string")
+   --         if highlight_string then
+    --          initiatives_selector:add_field( {'"highlight"("initiative"."name", ?)', highlight_string }, "name_highlighted")
+     --       end
             execute.view{
               module = "initiative",
-              view = "_list_ext_bs",
+              view = "_list_ext2_bs",
+              id = issue.id,
               params = {
                 issue = issue,
-                initiatives_selector = initiatives_selector,
-                highlight_initiative = for_initiative,
-                highlight_string = highlight_string,
-                no_sort = true,
-                limit = (for_listing or for_initiative) and 5 or nil,
-                hide_more_initiatives=false,
-                limit=25,
-                for_member = for_member
+      --          initiatives_selector = initiatives_selector,
+--                highlight_initiative = for_initiative,
+ --               highlight_string = highlight_string,
+--                no_sort = true,
+--                limit = (for_listing or for_initiative) and 5 or nil,
+--                hide_more_initiatives=false,
+--                limit=25,
+                list = list,
+--                for_member = for_member
               }
             }
           end }
