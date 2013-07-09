51ed8835044a3e8be1143937b6123f436ed66f40
root <root@debian>
2013-07-09 13:15:51 +0200

    Fix pg8-9

--- app/main/wizard/_page_bs9.lua
+++ app/main/wizard/_page_bs9.lua
@@ -39,14 +39,14 @@ ui.container{attr={class="row-fluid"},content=function()
   end }
 end }
                                        
-ui.container{attr={class="row-fluid",style="padding-top: 2em;"},content=function()
+ui.container{attr={class="row-fluid spaceline3"},content=function()
 ui.container{attr={class="span12 text-center"},content=function()
             --------------------------------------------------------      
             --contenuto specifico della pagina wizard    
              ui.form
                     {
                         method = "post",
-                        attr={id="wizardForm"..page,style="height:100%"},
+                        attr={id="wizardForm"..page,class="inline-block"},
                         module = 'wizard',
                         view = btnBackView,
                         params={
@@ -82,72 +82,25 @@ ui.container{attr={class="span12 text-center"},content=function()
                               trace.debug("[wizard] name="..k.name.." | value="..k.value)
                               end
                             end
-                      
-                        ui.container{attr={class="span2 text-center"},content=function()
-                        end}
-                        ui.container{attr={class="span8 text-center"},content=function()
-                        
+
+                      ui.container{attr={class="span12 text-center"},content=function()
+                        ui.container{attr={class="span10 offset1 text-center"},content=function()
                           ui.container{attr={class="row-fluid"},content=function()
-                              
-                             ui.container{attr={class="span12 "},content=function()
-                             
-                                 ui.container{attr={class="row-fluid"},content=function()
-                                 
-                                      ui.container{attr={class="span12 "},content=function()
-                                          
-                                         
-                             --contenuto
-                                         ui.tag{
-                                               tag="div",
-                                               attr={style="row-fluid"},
-                                               content=function()  
-                                               
-                                                   
-                                                 ui.container{attr={class="span12 text-center"},content=function()
-                                   
-                                                        ui.container
-                                                        {
-                                                            attr={style="width: 10em; position: relative; float: left;"},
-                                                            content=function()
-                                                             ui.tag{
-                                                                tag="p",
-                                                                attr={style="text-align: right; float: right; font-size: 20px;"},
-                                                               content=  _"Draft text"
-                                                              }   
-                                                            
-                                                             ui.tag{
-                                                               tag="p",
-                                                               attr={style="float: left; position: relative; text-align: right;  font-style: italic;font-size:15px;"},
-                                                               content=  _"Draft note"
-                                                              }   
-                                                              
-                                                            end
-                                                            
-                                                         }   
-                                                        ui.tag
-                                                           {
-                                                                tag="textarea",
-                                                                attr={id="draft",name="draft",style="resize: none;float: left; font-size: 23px; height: 43em; margin-left: 15px; width: 59%;"},
-                                                                content=function()
-                                                                end
-                                                                
-                                                           }
-                                                end }--fine content
-                                    
-                                               end}--fine div
-                      
-                                              end}  
-                                       end}
-                                
-                                 end}
-                                end}
-                                
-                              
-                            
-                             end}   
-                             ui.container{attr={class="span2 text-center"},content=function()
-                             end}  
-                           
+                            ui.container{attr={class="span6 text-right draft"},content=function()
+                              ui.tag{tag="p",content=  _"Draft text"}
+                              ui.tag{tag="em",content=  _"Draft note"}
+                            end }
+                            ui.container{attr={class="span6 draft"},content=function()
+                              ui.tag{
+                                tag="textarea",
+                                attr={id="draft",name="draft",style="height:100%;width:100%;resize:none;"},
+                                content=""
+                              }
+                            end }
+                          end }
+                        end }
+                      end }
+
                            end --fine contenuto
                    }--fine form
             --------------------------------------------------------
@@ -170,4 +123,6 @@ ui.container{attr={class="row-fluid btn_box_bottom"},content=function()
   end }
 end }
 
+ui.script{static = "js/jquery.equalheight.js"}
+ui.script{script = '$(document).ready(function() { equalHeight($(".draft")); $(window).resize(function() { equalHeight($(".draft")); }); }); ' }
 
