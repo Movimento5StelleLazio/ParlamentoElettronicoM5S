dd86ce37700be5ca37abb03623c287bfce3cd883
Vincenzo Abate <gogolander@gmail.com>
2014-08-10 12:43:27 +0200

    push

--- app/main/initiative/_show_bs.lua
+++ app/main/initiative/_show_bs.lua
@@ -323,43 +323,41 @@ elseif vote_comment_able then
 end 
 
 
-    ui.container{ attr = { class = "row-fluid spaceline2"}, content = function()
-    ui.container{ attr = { class = "span12 well-inside paper"}, content = function()
+ui.container{ attr = { class = "row-fluid spaceline2"}, content = function()
+	ui.container{ attr = { class = "span12 well-inside paper"}, content = function()
 
-                 ui.container{ attr = { class = "span4 offset1 spaceline"}, content = function()
-                      ui.heading{ level=2, content= "Se la proposta è passata per la fase di votazione, clicca  sul pulsante per votare:"}
-end }
-                 ui.container{ attr = { class = "span2 spaceline"}, content = function()
-ui.image{ static="svg/arrow-right.svg"}
-end }
-                    ui.container{ attr = { class = "span5"}, content = function()
-    ui.container{ attr = { class = "row-fluid spaceline-bottom"}, content = function()
+		ui.container{ attr = { class = "span4 offset1 spaceline"}, content = function()
+			ui.heading{ level=2, content= "Se la proposta è passata per la fase di votazione, clicca  sul pulsante per votare:"}
+		end }
+		ui.container{ attr = { class = "span2 spaceline"}, content = function()
+			ui.image{ static="svg/arrow-right.svg"}
+		end }
+			
+		ui.container{ attr = { class = "span5"}, content = function()
+			ui.container{ attr = { class = "row-fluid spaceline-bottom"}, content = function()
+				ui.link {        
+					attr = { id = "issue_see_det_"..issue.id},
+					module = "vote",
+					view = "list",
+					id = issue.id,
+					params = {issue_id = issue.id },
+					content = function()
+					ui.container{ attr = { class = "span6  btn btn-primary "}, content = function()
+						ui.container{ attr = { class = "row-fluid"}, content = function()
+							ui.container{ attr = { class = "span6"}, content = function()
+								ui.image{static="png/voting.png"}
+							end }
 
-                    ui.container{ attr = { class = "span6  btn btn-primary "}, content = function()
-    ui.container{ attr = { class = "row-fluid"}, content = function()
- ui.container{ attr = { class = "span6"}, content = function()
-ui.image{static="png/voting.png"}
+							ui.container{ attr = { class = "span6 text-center spaceline"}, content = function()									
+										ui.heading{ level=2, attr = { class = "spaceline"}, content=_"Vote now"}
+								end }
+							end }
+						end }
+					end }
+				end }
+		end }
+	end }
 end }
-
-ui.container{ attr = { class = "span6 text-center spaceline"}, content = function()
-          ui.link {        
-            attr = { id = "issue_see_det_"..issue.id},
-            module = "vote",
-            view = "list",
-            id = issue.id,
-            params = {issue_id = issue.id 
-              
-            },
-            content = function()
-              ui.heading{ level=2, attr = { class = "spaceline"}, content=_"Vote now"}
-          end }
-          end }
-          end }
-          end }
-          end }
-            end }
-          end }
-            end }
 if app.session:has_access("authors_pseudonymous") then
     ui.container{ attr = { class = "content" }, content = function()
       ui.tag{
@@ -564,7 +562,7 @@ ui.container{attr={class="row-fluid"}, content=function()
 
     execute.view{
       module = "draft",
-      view = "_show",
+      view = "show",
       params = {
         draft = initiative.current_draft
       }
