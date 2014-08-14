94141d5e13415020f31abb0fdb0923b3937d4883
Vincenzo Abate <gogolander@gmail.com>
2014-08-14 15:39:09 +0200

    push

--- app/main/initiative/show.lua
+++ app/main/initiative/show.lua
@@ -1,4 +1,7 @@
-local initiative = Initiative:by_id(param.get_id())
+local initiative = Initiative:by_id(param.get("initiative_id", atom.integer))
+if not initiative then
+	initiative = Initiative:by_id(param.get_id())
+end
 
 execute.view{
   module = "initiative", view = "_show_bs", params = {
